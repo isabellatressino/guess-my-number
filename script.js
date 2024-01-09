eba1bcaeca6878b4fcf56eadacaa815a7262c126
Isabella <117479295+isabellatressino@users.noreply.github.com>
2024-01-09 17:49:52 -0300

    Update script.js
    Adicionando um evento para a tecla "Enter" e comentando o código

--- script.js
+++ script.js
@@ -1,39 +1,47 @@
 "use strict";
 
+// Seleção dos elementos html
 const buttonCheck = document.querySelector(".check");
 const buttonAgain = document.querySelector(".again");
 const body = document.querySelector("body");
+const inputGuess = document.querySelector(".guess");
 
+// Inicialização de variáveis
 let secretNumber = Math.trunc(Math.random() * 100) + 1;
 let score = 20;
 let highScore = 0;
 
+// Função para exibir as mensagens
 const displayMessage = function (message) {
 	document.querySelector(".message").textContent = message;
 };
 
+// Função para atualizar o score
 const updateScore = function (sc) {
 	document.querySelector(".score").textContent = sc;
 };
 
+// Função para atualizar o highscore
 const updateHighScore = function (hs) {
 	document.querySelector(".highscore").textContent = hs;
 };
 
+// Função para habilitar/desabilitar o botao e input de adivinhação
 const toggleDisableButtons = function () {
-	const guessButton = document.querySelector(".guess");
+	const guess = document.querySelector(".guess");
 	const checkButton = document.querySelector(".check");
 
-	guessButton.disabled = !guessButton.disabled;
+	guess.disabled = !guess.disabled;
 	checkButton.disabled = !checkButton.disabled;
 };
 
-buttonCheck.addEventListener("click", function () {
-	const guess = Number(document.querySelector(".guess").value);
-
+// Função para verificar o número inserido pelo jogador
+const checkNumber = function (guess) {
 	if (!guess) {
+		// Se o palpite não for um número
 		displayMessage("⛔ No number!");
 	} else if (guess === secretNumber) {
+		// Se o palpite for correto
 		displayMessage("🎉 Correct Number!");
 
 		document.querySelector(".number").textContent = secretNumber;
@@ -47,21 +55,32 @@ buttonCheck.addEventListener("click", function () {
 
 		toggleDisableButtons();
 	} else if (guess > secretNumber && score > 1) {
+		// Se o palpite for muito alto
 		displayMessage("📈 Too high!");
 		score--;
 	} else if (guess < secretNumber && score > 1) {
+		// Se o palpite for muito baixo
 		displayMessage("📉 Too Low!");
 		score--;
 	} else {
+		// Se o jogador perder o jogo
 		body.style.backgroundColor = "#da0909d3";
 		displayMessage("💥 You lost the game");
 		score = 0;
 		toggleDisableButtons();
 	}
 	updateScore(score);
+};
+
+// Evento para o botão check
+buttonCheck.addEventListener("click", function () {
+	const guess = Number(document.querySelector(".guess").value);
+	checkNumber(guess);
 });
 
+// Evento para recomeçar
 buttonAgain.addEventListener("click", function () {
+	// Função que reinicia as variáveis e conteúdos
 	body.style.backgroundColor = "#222";
 	document.querySelector(".guess").value = "";
 	document.querySelector(".number").textContent = "?";
@@ -72,4 +91,11 @@ buttonAgain.addEventListener("click", function () {
 	updateScore(score);
 	displayMessage("Start guessing...");
 	toggleDisableButtons();
+});
+
+// Evento para a tecla enter
+inputGuess.addEventListener("keydown", function (e) {
+	if (e.key === "Enter") {
+		checkNumber(Number(inputGuess.value));
+	}
 });
